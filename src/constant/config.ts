fc7c1653b91efb703f82526eae3c102a7f536dbb
Yuri Mish <y.mish@otk.in.ua>
2024-07-17 17:44:36 +0300

    fix: seo2

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: 'Бухгалтерські послуги (м. Київ)',
-  description: 'Повний спектр бухгалтерськийх послуг у Києві',
+  description: 'Повний спектр бухгалтерських послуг у Києві',
   url: 'https://buholga.vercel.app',
 };
