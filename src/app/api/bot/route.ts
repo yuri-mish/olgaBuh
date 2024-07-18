b10f143ae60c235b511429c1abc4f8c92f651352
Yuri Mish <y.mish@otk.in.ua>
2024-07-18 18:36:04 +0300

    feat: bot1

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -3,6 +3,7 @@ export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
 import { Bot, webhookCallback } from 'grammy';
+//import { sql } from '@vercel/postgres';
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -10,6 +11,20 @@ if (!token)
   throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.');
 
 const bot = new Bot(token);
+
+bot.command('start', async (ctx) => {
+  //const message = ctx.update.message?.text ?? '';
+  //const {id,first_name, username='', language_code='' } = ctx.update.message?.from;
+  await bot.api.sendMessage(200000970, 'Hi!  ' || JSON.stringify(ctx));
+  // if (message === '/start') {
+  //   try {
+  //     await sql`INSERT INTO tg-users (id,first_name, username, language_code ) VALUES (${id}, ${first_name},${username}, ${language_code} );`;
+  //   } catch (error) {
+  //     return bot.NextResponse.json({ error }, { status: 500 });
+  //   }
+  // }
+});
+
 bot.on('message:text', async (ctx) => {
   await ctx.reply(JSON.stringify(ctx)); //.message.text);
 });
